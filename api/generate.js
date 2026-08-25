3c5880906a59435d0239cd731ec8ab75bca3fb33
oloruntobiopeyemi21-commits <oloruntobiopeyemi21@gmail.com>
2026-08-25 22:56:23 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "meta-llama/llama-3.1-8b-instruct:free",
+        model: "meta-llama/llama-3.3-70b-instruct:free",
         messages: [
           {
             role: "system",
